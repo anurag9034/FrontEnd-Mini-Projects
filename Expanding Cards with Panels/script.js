b1b1a5fe5c4a54d7935a6f4f3419633dcbdf3efd
anurag9034 <kamanurag5@gmail.com>
2022-04-18 09:32:31 -0700

    Commiting the Expanding Card project

---
+++ Expanding Cards with Panels/script.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll('.panel')
+
+// panels.forEach((panel) =>
+panels.forEach(function (panel) {
+  panel.addEventListener(
+    'click',
+    // () => {
+    function () {
+      removeActiveClasses()
+      panel.classList.add('active')
+    }
+  )
+})
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active')
+  })
+}
